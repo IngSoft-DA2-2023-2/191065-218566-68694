e60bae5407ebb7474dfe32c1204ebe2260899c35
Fabio Ramirez <fabio.uy@gmail.com>
2023-11-09 07:41:18 -0300

    Agregado manipulador de promociones en dll.

---
+++ ClothingStore/PromotionManager/IPromotionManager.cs
@@ -0,0 +1,20 @@
+﻿using ClothingStore.Domain.Entities;
+using PromotionManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionManager
+{
+    public interface IPromotionManager
+    {
+        public void PromotionDllLoad(string pathToFile);
+
+        public void PromotionDllUnload(string filename);
+
+        public void RunPromotions(ShoppingCart sp);
+
+    }
+}
